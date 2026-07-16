1ccdbfadb08eb2ed1b11433d0d8d73399fad5723
somchet373 <somchet2005@gmail.com>
2026-07-16 19:35:17 +0700

    แก้error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,7 @@
 import type { Metadata } from "next";
-import { Geist } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import NotificationManager from "@/components/NotificationManager"; // 1. Import เข้ามา
-
-const geist = Geist({
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "MyMedicine",
@@ -20,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="th">
-      <body className={`${geist.className} antialiased bg-zinc-950 text-zinc-50`}>
+      <body className="antialiased bg-zinc-950 text-zinc-50">
         {/* 2. เรียกใช้งานระบบแจ้งเตือน (ทำงานเบื้องหลัง) */}
         <NotificationManager />
 
